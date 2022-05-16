279b0f7518b8c8153a0cc0a3e1186d32637dce32
Toby Thomas <Toby@TobysMBTouchbar.modem>
2022-05-16 19:40:34 +1000

    16/05/22

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+// timer
+
 window.onload = function () {
   
     var seconds = 00; 
@@ -196,37 +198,131 @@ function showCalendar(month, year) {
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
 }
-const txtBox = document.querySelector(".txtBox");
-const form = document.querySelector(".searchForm");
-const searchWord = document.querySelector(".search-word");
-const description = document.querySelector(".description");
-const dictionaryJson =
-  "https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json";
-function searchDict(e) {
-  e.preventDefault();
-  fetch(dictionaryJson)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      e.preventDefault;
-      let word = txtBox.value;
-      console.log(txtBox.value);
-      let searchKeyword = word.toUpperCase();
-      Object.keys(data).forEach(function(key) {
-        if (key === searchKeyword) {
-          searchWord.innerHTML = searchKeyword;
-          if(searchKeyword === data[key]){
-            description.innerHTML = "Not found";
-          }
-          else{
-             description.innerHTML = data[key];
-          }
-          
-          console.log(data[key]);
-        }
-      });
-    });
+
+
+//Dictionary
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+const btn = document.getElementById("search-btn");
+btn.addEventListener("click", () => {
+	let inpWord = document.getElementById("inp-word").value;
+	fetch(`${url}${inpWord}`)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			result.innerHTML = `
+  <div class="word">
+    <h3>${inpWord}</h3>
+    <button onclick="playSound()"> <i class="fas fa-volume-up"></i></button>
+  </div>
+  <div class="details">
+    <p>${data[0].meanings[0].partOfSpeech}</p>
+    <p>/${data[0].phonetic}/</p>
+  </div>
+  <p class="word-meaning">
+    ${data[0].meanings[0].definitions[0].definition}
+  </p>
+  <p class="word-example">${
+			data[0].meanings[0].definitions[0].example || ""
+		}</p>
+  `;
+			sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+		})
+		.catch((error) => {
+			result.innerHTML = `<h3 class="error">Couldn't Find The Word!</h3>`;
+		});
+});
+function playSound() {
+	sound.play();
 }
 
-form.addEventListener("submit", searchDict);
+
+//Pomodoro timer
+var pomodoro = {
+    started : false,
+    minutes : 0,
+    seconds : 0,
+    fillerHeight : 0,
+    fillerIncrement : 0,
+    interval : null,
+    minutesDom : null,
+    secondsDom : null,
+    fillerDom : null,
+    init : function(){
+      var self = this;
+      this.minutesDom = document.querySelector('#minutes');
+      this.secondsDom = document.querySelector('#seconds');
+      this.fillerDom = document.querySelector('#filler');
+      this.interval = setInterval(function(){
+        self.intervalCallback.apply(self);
+      }, 1000);
+      document.querySelector('#work').onclick = function(){
+        self.startWork.apply(self);
+      };
+      document.querySelector('#shortBreak').onclick = function(){
+        self.startShortBreak.apply(self);
+      };
+      document.querySelector('#longBreak').onclick = function(){
+        self.startLongBreak.apply(self);
+      };
+      document.querySelector('#stop').onclick = function(){
+        self.stopTimer.apply(self);
+      };
+    },
+    resetVariables : function(mins, secs, started){
+      this.minutes = mins;
+      this.seconds = secs;
+      this.started = started;
+      this.fillerIncrement = 200/(this.minutes*60);
+      this.fillerHeight = 0;  
+    },
+    startWork: function() {
+      this.resetVariables(25, 0, true);
+    },
+    startShortBreak : function(){
+      this.resetVariables(5, 0, true);
+    },
+    startLongBreak : function(){
+      this.resetVariables(15, 0, true);
+    },
+    stopTimer : function(){
+      this.resetVariables(25, 0, false);
+      this.updateDom();
+    },
+    toDoubleDigit : function(num){
+      if(num < 10) {
+        return "0" + parseInt(num, 10);
+      }
+      return num;
+    },
+    updateDom : function(){
+      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+      this.fillerDom.style.height = this.fillerHeight + 'px';
+    },
+    intervalCallback : function(){
+      if(!this.started) return false;
+      if(this.seconds == 0) {
+        if(this.minutes == 0) {
+          this.timerComplete();
+          return;
+        }
+        this.seconds = 59;
+        this.minutes--;
+      } else {
+        this.seconds--;
+      }
+      this.updateDom();
+    },
+    timerComplete : function(){
+      this.started = false;
+      this.fillerHeight = 0;
+    }
+};
+window.onload = function(){
+  pomodoro.init();
+};
